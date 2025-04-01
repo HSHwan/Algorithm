62d0984dce813c65ac3ffd5f036342b7cd03c00f
HSHwan <tnghks2866@gmail.com>
2025-04-01 16:46:40 +0900

    [Gold IV] Title: N-Queen, Time: 1636 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9663. N－Queen/N－Queen.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define MAX 15
+
+using namespace std;
+
+int queenNum;
+int board[MAX][MAX];
+bool col[MAX], leftDiag[2 * MAX], rightDiag[2 * MAX];
+int nQueensNum = 0;
+
+void findNQueenCase(int queen) {
+    if (queen == queenNum) {
+        nQueensNum++;
+        return;
+    }
+    for (int i = 0; i < queenNum; i++) {
+        if (col[i] || leftDiag[queen + i] || rightDiag[queenNum + queen - i])
+            continue;
+        col[i] = true;
+        leftDiag[queen + i] = true;
+        rightDiag[queenNum + queen - i] = true;
+        findNQueenCase(queen + 1);
+        col[i] = false;
+        leftDiag[queen + i] = false;
+        rightDiag[queenNum + queen - i] = false;
+    }
+}
+
+int main() {
+    FAST_IO
+    cin >> queenNum;
+    findNQueenCase(0);
+    cout << nQueensNum;
+}
