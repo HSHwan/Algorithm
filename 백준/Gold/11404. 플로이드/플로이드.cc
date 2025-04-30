2a4071bbe8beeb4ebe020ce42cc142e1cbe47327
HSHwan <tnghks2866@gmail.com>
2025-04-30 15:36:48 +0900

    [Gold IV] Title: 플로이드, Time: 20 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Gold/11404. 플로이드/플로이드.cc
+++ 백준/Gold/11404. 플로이드/플로이드.cc
@@ -6,13 +6,13 @@
 
 using namespace std;
 
-int cityNum, cost[MAX][MAX][MAX];
+int cityNum, cost[MAX][MAX];
 
 void solveAllTravelCost() {
     for (int k = 1; k <= cityNum; k++) {
         for (int i = 1; i <= cityNum; i++) {
             for (int j = 1; j <= cityNum; j++) {
-                cost[k][i][j] = min(cost[k - 1][i][j], cost[k - 1][i][k] + cost[k - 1][k][j]);
+                cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j]);
             }
         }
     }
@@ -21,7 +21,7 @@ void solveAllTravelCost() {
 void printCostTable() {
     for (int i = 1; i <= cityNum; i++) {
         for (int j = 1; j <= cityNum; j++) {
-            cout << (cost[cityNum][i][j] != INF ? cost[cityNum][i][j] : 0) << ' ';
+            cout << (cost[i][j] != INF ? cost[i][j] : 0) << ' ';
         }
         cout << '\n';
     }
@@ -31,13 +31,13 @@ int main() {
     FAST_IO
     int busNum;
     cin >> cityNum >> busNum;
-    fill(&cost[0][1][1], &cost[0][cityNum][cityNum + 1], INF);
+    fill(&cost[1][1], &cost[cityNum][cityNum + 1], INF);
     for (int i = 0; i < busNum; i++) {
         int a, b, c;
         cin >> a >> b >> c;
-        cost[0][a][b] = min(cost[0][a][b], c);
+        cost[a][b] = min(cost[a][b], c);
     }
-    for (int i = 1; i <= cityNum; i++)  cost[0][i][i] = 0;
+    for (int i = 1; i <= cityNum; i++)  cost[i][i] = 0;
 
     solveAllTravelCost();
     printCostTable();
