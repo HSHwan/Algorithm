ca5b0c03c243c8af3580bcd7594eec6458c31ce3
HSHwan <tnghks2866@gmail.com>
2025-05-14 14:04:55 +0900

    [Gold V] Title: ABCDE, Time: 44 ms, Memory: 2192 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.cc
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <cstring>
+
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+
+using namespace std;
+
+int v, e;
+vector<vector<int>> graph;
+vector<bool> visited;
+bool exists = false;
+
+void dfs(int v, int cnt) {
+    if (exists) return;
+
+    visited[v] = true;
+    if (cnt == 4) {
+        exists = true;
+        return;
+    }
+    for (int nxt : graph[v]) {
+        if (visited[nxt])   continue;
+        dfs(nxt, cnt + 1);
+    }
+    visited[v] = false;
+}
+
+int main() {
+    FAST_IO
+    cin >> v >> e;
+    graph.resize(v);
+    for (int i = 0; i < e; i++) {
+        int u, v;
+        cin >> u >> v;
+        graph[u].push_back(v);
+        graph[v].push_back(u);
+    }
+
+    visited.assign(v, false);
+    for (int i = 0; i < v; i++) {
+        if (!graph[i].empty()) {
+            visited[i] = true;
+            dfs(i, 0);
+        }  
+        if (exists) break;
+    }
+
+    if (exists) cout << 1;
+    else        cout << 0;
+}
