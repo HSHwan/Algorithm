02c9359eca3aba01ee212bd22a1107f9f64585c1
HSHwan <tnghks2866@gmail.com>
2025-10-02 14:42:53 +0900

    [Silver IV] Title: 수들의 합 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
+++ 백준/Silver/2003. 수들의 합 2/수들의 합 2.cc
@@ -1,29 +1,25 @@
 #include <iostream>
-#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
-#define MAX_LEN 10000
-
 using namespace std;
 
-int arr[MAX_LEN], arrSum[MAX_LEN + 1] = {0, };
-
 int main() {
-    FAST_IO
-
-    int arrLen, target;
-    cin >> arrLen >> target;
-
-    for (int i = 0; i < arrLen; i++) {
-        cin >> arr[i];
-        arrSum[i + 1] = arr[i] + arrSum[i];
+    int N,M;
+    cin >> N >> M;
+    int A[N];
+    for (int i=0;i<N;i++) {
+        cin >> A[i];
     }
-
-    int count = 0;
-    for (int i = 0; i < arrLen; i++) {
-        for (int j = i + 1; j <= arrLen; j++) {
-            arrSum[j] -= arrSum[i];
-            count += (target == arrSum[j]);
+    int j=0;
+    int sum=0;
+    int num=0;
+    for (int i=0;i<N;i++) {
+        while(j!=N && sum<M) {
+            sum+=A[j];
+            j++;
         }
+        if (sum==M) {
+            num++;
+        }
+        sum-=A[i];
     }
-
-    cout << count;
-}
+    cout << num;
+} 
