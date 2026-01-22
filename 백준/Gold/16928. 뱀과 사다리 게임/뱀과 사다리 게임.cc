5b80d424b94bd472cca247564cc5ad7be49a8f40
HSHwan <tnghks2866@gmail.com>
2026-01-22 16:40:32 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.cc
@@ -10,18 +10,12 @@ int main() {
     int n, m;
     cin >> n >> m;
 
-    vector<int> ladder(MAX, 0);
-    for (int i = 0; i < n; i++) {
+    int move_num = n + m;
+    vector<int> move(MAX, 0);
+    for (int i = 0; i < move_num; i++) {
         int x, y;
         cin >> x >> y;
-        ladder[x] = y;
-    }
-
-    vector<int> snake(MAX, 0);
-    for (int i = 0; i < m; i++) {
-        int u, v;
-        cin >> u >> v;
-        snake[u] = v;
+        move[x] = y;
     }
 
     vector<int> board(MAX, 2 * MAX);
@@ -32,11 +26,8 @@ int main() {
             for (int j = 1; j <= 6; j++) {
                 int nxt = i + j;
                 if (nxt >= MAX)   break;
-                if (ladder[nxt]) {
-                    board[ladder[nxt]] = min(board[ladder[nxt]], board[i] + 1);
-                }
-                else if (snake[nxt]) {
-                    board[snake[nxt]] = min(board[snake[nxt]], board[i] + 1);
+                if (move[nxt]) {
+                    board[move[nxt]] = min(board[move[nxt]], board[i] + 1);
                 }
                 else {
                     board[nxt] = min(board[nxt], board[i] + 1);
