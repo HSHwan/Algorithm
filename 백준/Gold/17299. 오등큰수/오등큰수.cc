62572137f47b7314a3faf3500f7f26c9c6b4ba80
HSHwan <tnghks2866@gmail.com>
2026-01-24 00:07:33 +0900

    [Gold III] Title: 오등큰수, Time: 256 ms, Memory: 10572 KB -BaekjoonHub

---
+++ 백준/Gold/17299. 오등큰수/오등큰수.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <deque>
+#include <stack>
+
+using namespace std;
+
+int FAn[1000001] = {0, };
+
+int main(){
+	ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+	int N, num;
+	stack<int> perm;
+	deque<int> deq, nge;
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++){
+		cin >> num;
+		perm.push(num);
+		FAn[num]++;
+	}
+
+	while(!perm.empty()){
+		while(!deq.empty() && FAn[perm.top()] >= FAn[deq.front()]){
+			deq.pop_front();
+		}
+		if(deq.empty())	nge.push_front(-1);
+		else	nge.push_front(deq.front());
+		deq.push_front(perm.top());
+		perm.pop();
+	}
+
+	for (auto& nge_n : nge){
+		cout << nge_n << ' ';
+	}
+}
