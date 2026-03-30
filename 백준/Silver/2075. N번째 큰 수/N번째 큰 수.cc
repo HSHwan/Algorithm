aeb69c5d1dbd8882054539befa1a3a38f937dd4c
HSHwan <tnghks2866@gmail.com>
2026-03-30 16:58:06 +0900

    [Silver III] Title: N번째 큰 수, Time: 272 ms, Memory: 10944 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.cc
@@ -1,22 +1,41 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
+#include <queue>
 #define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 using namespace std;
+
+typedef vector<int>::iterator vii;
+
+struct comp {
+    bool operator()(vii a, vii b) {
+        return *a < *b;
+    }
+};
 
 int main() {
     FAST_IO
 
     int N;
     cin >> N;
-
-    int N_squared = N * N;
-    vector<int> arr(N_squared);
-    for (int i = 0; i < N_squared; i++) {
-        cin >> arr[i];
+    
+    vector<vector<int>> matrix(N, vector<int>(N));
+    for (int i = N - 1; i >= 0; i--) {
+        for (int j = 0; j < N; j++) {
+            cin >> matrix[j][i];
+        }
     }
-
-    sort(arr.begin(), arr.end(), greater<int>());
-    cout << arr[N - 1];
+    priority_queue<vii, vector<vii>, comp> pq;
+    for (int i = 0; i < N; i++) {
+        pq.push(matrix[i].begin());
+    }
+    int count = 1;
+    while (count < N) {
+        vii nxt = pq.top();
+        nxt++;
+        pq.push(nxt);
+        pq.pop();
+        count++;
+    }
+    cout << *pq.top();
 }
