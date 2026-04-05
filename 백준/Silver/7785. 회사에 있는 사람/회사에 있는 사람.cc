1ec371b2dda616af294c1ff6a2aac9aef71780b5
HSHwan <tnghks2866@gmail.com>
2026-04-05 23:20:08 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 76 ms, Memory: 9948 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.cc
@@ -1,30 +1,23 @@
 #include <iostream>
-#include <string>
-#include <map>
-#include <vector>
-#include <algorithm>
+#include <set>
+#define FAST_IO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 
 using namespace std;
 
 int main() {
-    ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
-    int n;
-    cin >> n;
-    
-    map<string, bool> logs;
-    string name, log;
-    for (int i = 0; i < n; i++){
-        cin >> name >> log;
-        if (log == "enter") logs[name] = true;
-        else    logs[name] = false;
+    FAST_IO
+    set<string, greater<string>> logs;
+    int N;
+    cin >> N;
+
+    for (int i = 0; i < N; i++) {
+        string name, action;
+        cin >> name >> action;
+        if (action == "enter")  logs.insert(name);
+        else logs.erase(name);
     }
 
-    vector<string> logs_vec;
-    for(auto& log : logs){
-        if (log.second) logs_vec.push_back(log.first);
+    for (auto log : logs) {
+        cout << log << '\n';
     }
-    
-    sort(logs_vec.rbegin(), logs_vec.rend());
-    
-    for(auto& log : logs_vec)   cout << log << '\n';
 }
