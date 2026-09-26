7f7ee50665b219cfcb1794deb868c87aa2ba4fe3
HSHwan <tnghks2866@gmail.com>
2026-09-26 13:14:21 +0900

    [level 3] Title: 입국심사, Time: 11.66 ms, Memory: 6.94 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.cpp
+++ 프로그래머스/3/43238. 입국심사/입국심사.cpp
@@ -1,10 +1,12 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
 long long solution(int n, vector<int> times) {
-    long long low = 1, high = 1e15;
+    long long low = 1;
+    long long high = (long long)*max_element(times.begin(), times.end()) * n;
     
     while (low <= high) {
         long long mid = (low + high) / 2;
